d470913132702d27136748e60b548a20afc73587
Michael Barbachov <mbarbachov@gmail.com>
2020-06-12 14:50:09 -0700

    adding more projects

--- Javascript_games/animations/audio visual/app.js
+++ Javascript_games/animations/audio visual/app.js
@@ -4,11 +4,14 @@ let begin = false;
 let myButton1 = document.getElementById('play');
 let myButton2 = document.getElementById('reload')
 let myButton3 = document.getElementById('file-button');
+let myButton4 = document.getElementById('replay-song');
 let songTitle = document.getElementById('name');
 
+// getting our audio tag
 let audio = document.getElementById('audio');
 
 let start = function() {
+    // initialize our visualizer by drawing the bar and starting the analyser
     let ctx = new AudioContext();
     let analyser = ctx.createAnalyser();
     let audioSrc = ctx.createMediaElementSource(audio);
@@ -30,7 +33,7 @@ let start = function() {
         capStyle = '#fff',
         meterNum = 800 / (10 + 2), //count of the meters
         capYPositionArray = []; ////store the vertical position of hte caps for the preivous frame
-    ctx = canvas.getContext('2d'),
+        ctx = canvas.getContext('2d'),
         gradient = ctx.createLinearGradient(0, 0, 0, 300);
 
     let c1 = getRandomColor(), c2 = getRandomColor(), c3 = getRandomColor();
@@ -78,6 +81,7 @@ function getRandomColor() {
 }
 
 function preparation() {
+    //  start tge visualizer unless the audio tag has no src
     if (begin === true) {
         if (audio.paused) {
             audio.play();
@@ -86,20 +90,11 @@ function preparation() {
         }
     }
     else {
-        let songsArray = [], genres = [["memes", 8], ["russian", 9]], dir, fileCount;
-
-        dir = genres[Math.floor(Math.random() * genres.length)];
-        fileCount = dir[1];
-
-        for (let i = 0; i < fileCount; i++) {
-            songsArray.push("music/" + dir[0] + "/song" + i.toString() + ".mp3");
-        }
-
-        let source = songsArray[Math.floor(Math.random() * songsArray.length)];
         let audio = document.getElementById("audio");
-        audio.src = source;
+        let src = generateSource();
+        audio.src = src;
 
-        songTitle.textContent = source;
+        songTitle.textContent = src;
     }
     start();
     begin = true;
@@ -107,23 +102,41 @@ function preparation() {
     audio.play();
     audio.addEventListener("ended", function () {
         begin = false;
-        location.reload()
+        let src = generateSource();
+
+        audio.src = src;
+        audio.play()
+        songTitle.textContent = src;
     });
 }
 
+// generates a source for our audio
+function generateSource() {
+    let songsArray = [], genres = [["memes", 8], ["russian", 9]], dir, fileCount;
 
+    dir = genres[Math.floor(Math.random() * genres.length)];
+    fileCount = dir[1];
+
+    for (let i = 0; i < fileCount; i++) {
+        songsArray.push("music/" + dir[0] + "/song" + i.toString() + ".mp3");
+    }
+
+    return songsArray[Math.floor(Math.random() * songsArray.length)];
+}
+
+// adding an event listener to reload the page
 myButton2.onclick = function() {
     location.reload();
 };
 
-
+// adding an event listener for adding a file src for our audio tag
 myButton3.onclick = function() {
     document.getElementById("file-input").click();
 }
 
-
 let file = document.getElementById("file-input");
 
+// changing the audio src to the file that we received from the user
 file.onchange = function() {
     let files = this.files;
     console.log(this.files)
@@ -134,3 +147,17 @@ file.onchange = function() {
     myButton1.click();
 };
 
+// adding an event listener to replay the song that is playing
+myButton4.onclick = function () {
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+}
+
+class Test {
+    constructor(a) {
+        this.name = "Michael";
+        this._name = "Jhonny";
+    }
+    a = "ALex"
+}
